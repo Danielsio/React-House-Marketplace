d4f2de10967f5c4f5b0dac2be3ce5f39c04ac758
Danielsio <kingddd301@gmail.com>
2022-09-21 21:54:11 +0300

    added forgot password feture

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Explore from "./pages/Explore";
-import Offers from "./pages/Offers";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import ForgotPassword from "./pages/ForgotPassword";
-import Navbar from "./components/Navbar";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Explore from "./pages/Explore"
+import Offers from "./pages/Offers"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import ForgotPassword from "./pages/ForgotPassword"
+import Navbar from "./components/Navbar"
+import "react-toastify/dist/ReactToastify.css"
+import { ToastContainer } from "react-toastify"
+import PrivateRoute from "./components/PrivateRoute"
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Explore />}></Route>
-          <Route path="/offers" element={<Offers />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
-          <Route path="/sign-in" element={<SignIn />}></Route>
-          <Route path="/sign-up" element={<SignUp />}></Route>
-          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
       <ToastContainer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,22 +1,51 @@
-import { getAuth } from "firebase/auth";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { getAuth, updateProfile } from "firebase/auth"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { updateDoc, doc } from "firebase/firestore"
+import { db } from "../firebase.config"
+import { toast } from "react-toastify"
 
 function Profile() {
-  const auth = getAuth();
-  const navigate = useNavigate();
+  const auth = getAuth()
+  const navigate = useNavigate()
+
+  const [changeDetails, setChangeDetails] = useState(false)
 
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
-  });
+  })
 
-  const { name, email } = formData;
+  const { name, email } = formData
 
   const onLogout = () => {
-    auth.signOut();
-    navigate("/");
-  };
+    auth.signOut()
+    navigate("/")
+  }
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update display name in firebase
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        })
+
+        // update in firestore
+        const userRef = doc(db, "users", auth.currentUser.uid)
+        await updateDoc(userRef, { name })
+      }
+    } catch (error) {
+      toast.error("Couldn't update try again later.")
+    }
+  }
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
 
   return (
     <div className="profile">
@@ -26,8 +55,43 @@ function Profile() {
           Logout
         </button>
       </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit()
+              setChangeDetails((prevState) => !prevState)
+            }}
+          >
+            {changeDetails ? "done" : "edit profile"}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
-  );
+  )
 }
 
-export default Profile;
+export default Profile
